1a667e21f1c114e2300db95d9d45ca0e4c76ade1
John Jeyarathinam <john.gnanaraj@wearefmg.net>
2018-02-23 03:28:14 +0530

    profile update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,6 +6,7 @@ import { ProfileService } from '../services/profile.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
 
   constructor(private _profileService: ProfileService) { }
